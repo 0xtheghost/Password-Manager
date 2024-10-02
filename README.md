fc0f7cfb98bdcf21f8722072820d60531d8452bb
0xtheghost <mael.demons17@gmail.com>
2024-10-02 11:33:34 +0200

    Update v.1.5, Update Readme and example data.json

--- README.md
+++ README.md
@@ -1,16 +1,70 @@
-# Password-Manager
+# Password-Manager by 0xtheghost
 ## Table of Contents
 
 - [About](#About)
 - [Features](#Features)
-- [Prerequisites](#Prerequisites)
-- [Installation](#Installation)
+- [Setup & Installation](#Setup&Installation)
 - [Usage](#Usage)
 - [License](#License)
 
 ## About
 
-**Password-Manager** is a password management application implemented in Python, which uses the Cryptography library to securely encrypt and decrypt data, as well as Colorama for coloring messages in the console. The program uses a command-line interface (CLI) to interact with the user, and offers options for storing, displaying and managing passwords or sensitive data.
+**Password-Manager** is a command-line interface (CLI) Password Manager that securely stores and manages passwords using encryption techniques. It utilizes `bcrypt` for hashing and verifying the master password, and `cryptography.fernet` for encrypting and decrypting data. It allows users to add, view, update, delete, and manage sensitive information, protected by a master password.
+
+The program provides an interactive interface for managing sensitive information, stored in an encrypted file, with features such as key generation, password management, and JSON import/export.
+
+## Features
+
+- Master Password Protection: Secure your data with a hashed master password.
+- Encryption/Decryption: Uses Fernet encryption to secure sensitive data.
+- Key Management: Generate, load, and change encryption keys.
+- Data Management:
+    - Add or update data entries.
+    - View specific data entries or the entire dataset.
+    - Delete data entries.
+- Import/Export: Import and export data as JSON files.
+- Interactive CLI: Provides a simple menu-driven interface for managing data.
+
+## Setup & Installation
+1. Clone the repository:
+``` bash
+git clone https://github.com/0xtheghost/Password-Manager.git
+cd Password-Manager
+```
+2. Install the required dependencies:
+``` bash
+pip install -r requirements.txt
+```
+3. Run the application :
+``` bash
+python main.py
+```
+
+## Usage
+
+Upon starting the application, you will be prompted to enter the master password. If none exists, the program will guide you to set one.
+
+### Menu Options:
+1. Show Data: View all stored data or data at a specific path.
+2. Add/Update Data: Add new data or update existing data at a given path.
+3. Delete Data: Remove data from a specified path.
+4. Change Key: Generate a new encryption key and re-encrypt the data.
+5. Export to JSON: Save your data into a JSON file.
+6. Import from JSON: Load data from an existing JSON file and merge it with the current data.
+7. Quit: Exit the application.
+### How It Works
+- **Encryption Key:** Stored in a file (`key.key`), used to encrypt and decrypt your data.
+- **Data Storage:** Encrypted data is saved in `data.enc`. The content is a JSON structure that holds key-value pairs.
+- **Master Password:** The master password is hashed and stored in `password.hash` using bcrypt. It must be provided to unlock the manager and perform actions.
+### Security Considerations
+- **Key Management:** The key file is crucial for decrypting your data. Do not lose it, or you will lose access to your stored information.
+- **Password Security:** The master password is hashed using bcrypt. However, make sure to choose a strong password to prevent brute-force attacks.
+### Example
+Here’s a quick example of how to add, view, and update data:
+1. Start the program and authenticate with your master password.
+2. Select option `2` to add or update data.
+3. Provide a location path (e.g., `accounts/github`) and enter the value (e.g., `my-github-password`).
+4. To view the added data, select option `1` and input the same path (`accounts/github`).
 
 ## License
 This project is licensed under the Apache License 2.0 - see the [LICENSE](https://github.com/0xtheghost/Password-Manager/blob/main/LICENSE) file for details.
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import json
 import os
+import bcrypt
 from cryptography.fernet import Fernet
 from collections import OrderedDict
 from colorama import Fore
 
 class PasswordManager:
-    def __init__(self, key_file="key.key", encrypted_file='data.enc') -> None:
+    def __init__(self, key_file="key.key", encrypted_file='data.enc', password_file='password.hash') -> None:
         self.key_file = key_file
         self.encrypted_file = encrypted_file
+        self.password_file = password_file
         self.cipher = None
 
     def generate_key(self):
@@ -22,14 +24,54 @@ class PasswordManager:
             with open(self.key_file, 'rb') as file:
                 key = file.read()
                 self.cipher = Fernet(key)
-                print(Fore.CYAN + f"[Key loaded from : {Fore.GREEN}'{self.key_file}'{Fore.CYAN}]{Fore.RESET}")
+                print(Fore.CYAN+f"[Key loaded from : {Fore.GREEN}'{self.key_file}'{Fore.CYAN}]{Fore.RESET}")
         except FileNotFoundError:
             print(Fore.RED+f"Keyfile not found{Fore.RESET}")
-            choice = input(Fore.CYAN + f"Do you want to generate a new key? (y/n) :{Fore.GREEN} "+Fore.RESET)
+            choice = input(Fore.CYAN+f"Do you want to generate a new key? (y/n) : {Fore.GREEN}"+Fore.RESET)
             if choice.lower() == 'y':
                 self.generate_key()
             else:
-                print(Fore.RED + "As no key has been loaded, operations requiring a key will not be possible."+Fore.RESET)
+                print(Fore.RED+"As no key has been loaded, operations requiring a key will not be possible."+Fore.RESET)
+    
+    def set_password(self):
+        password = input(Fore.BLUE+"Enter new master password : "+Fore.RESET).encode()
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        with open(self.password_file, 'wb') as file:
+            file.write(hashed)
+        print(Fore.GREEN+"Master password set successfully."+Fore.RESET)
+
+    def verify_password(self):
+        """Verifies if the master password is correct."""
+        if not os.path.exists(self.password_file):
+            print(Fore.YELLOW+"No master password found. Please create one."+Fore.RESET)
+            self.set_password()
+        
+        password = input(Fore.BLUE+"Enter master password : "+Fore.RESET).encode()
+        with open(self.password_file, 'rb') as file:
+            hashed = file.read()
+
+        if bcrypt.checkpw(password, hashed):
+            print(Fore.GREEN+"Authentication successful."+Fore.RESET)
+            return True
+        else:
+            print(Fore.RED+"Authentication failed. Incorrect password."+Fore.RESET)
+            return False
+        
+    def change_key(self):
+        if os.path.exists(self.encrypted_file):
+            current_data = self.load_data() or {}
+        else:
+            current_data = {}
+
+        self.generate_key()
+
+        if current_data:
+            self.save_data(current_data)
+            print(Fore.GREEN+"Data re-encrypted with the new key.\n"+Fore.RESET)
+        else:
+            print(Fore.RED+"No data found to re-encrypt."+Fore.RESET)
+
+        input(Fore.MAGENTA+"\nPress enter to finish ")
 
     def save_data(self, data):
         if self.cipher is None:
@@ -84,7 +126,7 @@ class PasswordManager:
     def add_or_update_data_at_path(self, path, value):
         data = self.load_data() or {}
         parent, final_key = self.navigate_to_path(data, path)
-        parent[final_key] = value
+        parent[final_key] = str(value)
         print(Fore.CYAN+f"Add/update data at location {Fore.GREEN}'{path}'{Fore.CYAN} : {value}"+Fore.RESET)
         self.save_data(data)
 
@@ -128,6 +170,9 @@ class PasswordManager:
         return parent, keys[-1]
 
     def run_cli(self):
+        if not self.verify_password():
+            return
+        
         while True:
             os.system("cls")
             print(Fore.CYAN + """\n
@@ -140,36 +185,39 @@ class PasswordManager:
             """+ Fore.RESET)
             manager.load_key()
             print(Fore.MAGENTA+"\n====================================="+Fore.RESET)
-            print(Fore.GREEN + f"1. {Fore.CYAN}Add or update data"+Fore.RESET)
-            print(Fore.GREEN + f"2. {Fore.CYAN}Delete data"+Fore.RESET)
-            print(Fore.GREEN + f"3. {Fore.CYAN}View data"+Fore.RESET)
-            print(Fore.GREEN + f"4. {Fore.CYAN}Importing data from a {Fore.GREEN}.json{Fore.CYAN} file"+Fore.RESET)
-            print(Fore.GREEN + f"5. {Fore.CYAN}Export data to a {Fore.GREEN}.json{Fore.CYAN} file"+Fore.RESET)
-            print(Fore.GREEN + f"6. {Fore.CYAN}Quit"+Fore.RESET)
-            print(Fore.MAGENTA+"====================================="+Fore.RESET)
-            choice = input(Fore.CYAN+f"\nChoose an option {Fore.GREEN}(1-6){Fore.CYAN} : "+Fore.RESET)
+            print(Fore.GREEN + f"[1] - {Fore.CYAN}Show data"+Fore.RESET)
+            print(Fore.GREEN + f"[2] - {Fore.CYAN}Add/Update data"+Fore.RESET)
+            print(Fore.GREEN + f"[3] - {Fore.CYAN}Delete data"+Fore.RESET)
+            print(Fore.GREEN + f"[4] - {Fore.CYAN}Change key"+Fore.RESET)
+            print(Fore.GREEN + f"[5] - {Fore.CYAN}Export .json"+Fore.RESET)
+            print(Fore.GREEN + f"[6] - {Fore.CYAN}Import .json"+Fore.RESET)
+            print(Fore.GREEN + f"[7] - {Fore.CYAN}Quit"+Fore.RESET)
+            print(Fore.MAGENTA+"=====================================\n"+Fore.RESET)
+            choice = input(Fore.CYAN+"Choose an option : "+Fore.RESET)
             if choice == '1':
-                path = input(Fore.BLUE+"Enter data path (ex: email1/google) : "+Fore.RESET)
-                value = input(Fore.BLUE+"Entrez la valeur : "+Fore.RESET)
+                path = input(Fore.BLUE+"Enter the location path (leave empty for root): " + Fore.RESET)
+                os.system("cls")
+                self.show_data_at_path(path)
+            elif choice == '2':
+                path = input(Fore.BLUE + "Enter the location path: " + Fore.RESET)
+                value = input(Fore.BLUE + "Enter the value: " + Fore.RESET)
                 os.system("cls")
                 self.add_or_update_data_at_path(path, value)
-            elif choice == '2':
-                path = input(Fore.BLUE+"Enter the path to delete (ex: email1/google) : "+Fore.RESET)
+            elif choice == '3':
+                path = input(Fore.RED + "Enter the location path: " + Fore.RESET)
                 os.system("cls")
                 self.delete_data_at_path(path)
-            elif choice == '3':
-                path = input(Fore.BLUE+"Enter path to view data (leave empty to display all) : "+Fore.RESET)
-                os.system("cls")
-                self.show_data_at_path(path.strip() or None)
             elif choice == '4':
-                json_file = input(Fore.BLUE+"Enter the path of the .json file to be imported : "+Fore.RESET)
-                os.system("cls")
-                self.import_json(json_file)
+                self.change_key()
             elif choice == '5':
-                json_file = input(Fore.BLUE+"Enter path of .json file for export : "+Fore.RESET)
+                json_file = input(Fore.CYAN + "Enter the JSON file to export to: " + Fore.RESET)
                 os.system("cls")
                 self.export_json(json_file)
             elif choice == '6':
+                json_file = input(Fore.CYAN + "Enter the JSON file to import from: " + Fore.RESET)
+                os.system("cls")
+                self.import_json(json_file)
+            elif choice == '7':
                 os.system("cls")
                 print(Fore.MAGENTA+"""
  ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██╗
@@ -177,13 +225,10 @@ class PasswordManager:
 ██║  ███╗██║   ██║██║   ██║██║  ██║██████╔╝ ╚████╔╝ █████╗  ██║
 ██║   ██║██║   ██║██║   ██║██║  ██║██╔══██╗  ╚██╔╝  ██╔══╝  ╚═╝
 ╚██████╔╝╚██████╔╝╚██████╔╝██████╔╝██████╔╝   ██║   ███████╗██╗
- ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝
-                                                                     
-"""+Fore.RESET)
+ ╚═════╝  ╚═════╝  ╚═════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝"""+Fore.RESET)
                 break
             else:
-                os.system("cls")
-                print(Fore.RED+"Choix invalide, veuillez réessayer."+Fore.RESET)
+                print(Fore.RED+"Invalid option."+Fore.RESET)
 
 if __name__ == "__main__":
     manager = PasswordManager()
